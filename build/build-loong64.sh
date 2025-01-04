91bf0c897f32c8389a32f3a09f50f596cc51035c
Moe-hacker <moe-hacker@outlook.com>
2025-01-04 08:13:32 +0000

    Add build action

---
+++ build/build-loong64.sh
@@ -0,0 +1,19 @@
+sudo debootstrap --foreign --arch=loong64 --variant=buildd --include=debian-ports-archive-keyring --verbose --components=main --resolve-deps --extra-suites=unreleased unstable ./debian http://ftp.ports.debian.org/debian-ports
+sudo cp /usr/bin/qemu-loongarch64-static debian/usr/bin/
+cat <<EOF >>./debian/build.sh
+rm /etc/resolv.conf
+echo nameserver 1.1.1.1 > /etc/resolv.conf
+apt update
+apt install -y wget make clang git libseccomp-dev libcap-dev libc-dev binutils
+apt install -y upx
+git clone https://github.com/moe-hacker/cprintf
+cd cprintf
+./configure -s
+make
+strip cprintf
+tar -cvf ../loong64.tar ./cprintf ./LICENSE
+EOF
+sudo chmod +x ./debian/build.sh
+./ruri/ruri -p -a loong64 -q /usr/bin/qemu-loongarch64-static ./debian /debootstrap/debootstrap --second-stage
+sudo ./ruri/ruri -p -a loong64 -q /usr/bin/qemu-loongarch64-static ./debian /bin/sh /build.sh
+cp ./debian/loong64.tar ../loongarch64.tar
---
+++ build/build.sh
@@ -0,0 +1,25 @@
+rm /etc/resolv.conf
+echo nameserver 1.1.1.1 >/etc/resolv.conf
+echo https://dl-cdn.alpinelinux.org/alpine/edge/testing >>/etc/apk/repositories
+apk add wget make clang git libseccomp-dev libseccomp-static libcap-static libcap-dev xz-dev libintl libbsd-static libsemanage-dev libselinux-utils libselinux-static xz-libs zlib zlib-static libselinux-dev linux-headers libssl3 libbsd libbsd-dev gettext-libs gettext-static gettext-dev gettext python3 build-base openssl-misc openssl-libs-static openssl zlib-dev xz-dev openssl-dev automake libtool bison flex gettext autoconf gettext sqlite sqlite-dev pcre-dev wget texinfo docbook-xsl libxslt docbook2x musl-dev gettext gettext-asprintf gettext-dbg gettext-dev gettext-doc gettext-envsubst gettext-lang gettext-libs gettext-static
+apk add upx
+apk add lld
+mkdir output
+mkdir output2
+git clone https://github.com/moe-hacker/cprintf
+cd cprintf
+./configure -s
+make
+strip cprintf
+cp cprintf ../output2/cprintf
+cp LICENSE ../output2/LICENSE
+upx cprintf
+cp cprintf ../output/cprintf
+cp LICENSE ../output/LICENSE
+cd ..
+cd output
+tar -cvf ../$(uname -m).tar .
+cd ..
+cd output2
+tar -cvf ../$(uname -m)-noupx.tar .
+exit 0
